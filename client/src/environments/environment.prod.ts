16c3dd298b1ba5676c603f23d885abe1837e60f0
Zakaria Babahadji <zakaria.babahadji@gmail.com>
2025-02-10 15:18:00 -0500

    remove url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-15-222-253-152.ca-central-1.compute.amazonaws.com:3000/api',
-    socketUrl: 'http://ec2-15-222-253-152.ca-central-1.compute.amazonaws.com:3000',
+    serverUrl: '',
+    socketUrl: '',
 };
