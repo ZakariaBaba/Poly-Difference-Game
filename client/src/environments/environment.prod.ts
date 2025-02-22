be3fa18da69c853f363d9c177d46cbff6dbf1040
Zakaria Babahadji <zakaria.babahadji@gmail.com>
2025-02-22 15:46:01 -0500

    add url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl: '',
-    socketUrl: '',
+    serverUrl: 'http://ec2-35-182-69-251.ca-central-1.compute.amazonaws.com:3000/api',
+    socketUrl: 'http://ec2-35-182-69-251.ca-central-1.compute.amazonaws.com:3000',
 };
